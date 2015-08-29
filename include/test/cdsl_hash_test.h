e83a6975bfde9ee1dd3003cde1d7a75c7cdd6368
fritzprix <innocentevil0914@gmail.com>
2015-08-30 00:50:44 +0900

    - commit missing file

---
+++ include/test/cdsl_hash_test.h
@@ -0,0 +1,19 @@
+/*
+ * cdsl_hash_test.h
+ *
+ *  Created on: 2015. 8. 29.
+ *      Author: innocentevil
+ */
+
+#ifndef CDSL_HASH_TEST_H_
+#define CDSL_HASH_TEST_H_
+
+#include "cdsl.h"
+
+
+extern BOOL cdsl_hashDoTest(void);
+
+
+
+
+#endif /* CDSL_HASH_TEST_H_ */
