6a913deedd35b5ca7c4ff9cf8936407e41c4e643
DooWoong Lee <innocentevil0914@gmail.com>
2017-03-05 22:27:05 +0900

    0.1.5 (#13)
    * minor bug fix (#9)
    
    * 0.1.4 (#7)
    
    * added unit testing code for baremetal utility
    
    * improve hashtree collision handling.
    improve test coverage
    add random string sequence to be used in hashtree testing
    
    * erroneous header inclusion is fixed
    
    * fix dropped build dependency in 'cdsl_defs.h'
    
    * - simplify delete op. in plain binary search tree
    - format code along the k&r style
    
    * - working on AVL tree implmentation
    - working on API document (doxygen)
    
    * doxygen work for base_tree / avltree is done.
    implementing AVL tree
    
    * minor fix in header file (cdsl_avltree.h)
    
    Signed-off-by: fritzprix <innocentevil0914@gmail.com>

--- include/cdsl_avltree.h
+++ include/cdsl_avltree.h
@@ -15,6 +15,14 @@ extern "C" {
 #endif
 
 
+
+/*!
+ * \addtogroup avl-tree AVL Tree
+ * \brief AVL Tree (Recursive Version)
+ * @{
+ */
+
+
 #define cdsl_avltreeMaxDepth(root)                      tree_max_depth((base_treeRoot_t*) root)
 #define cdsl_avltreeTraverse(root, cb, order, arg)      tree_traverse((base_treeRoot_t*) root, (base_tree_callback_t) cb, order, arg)
 #define cdsl_avltreeTraverseTarget(root,cb,key, arg)    tree_traverse((base_treeRoot_t*) root, (base_tree_callback_t) cb, key, arg)
@@ -32,38 +40,193 @@ extern "C" {
 #define cdsl_avltreeDeleteMax(root)                     cdsl_avltreeDeleteMaxReplace(root, NULL, NULL)
 
 
-typedef struct avltree_node avltreeNode_t;
-typedef struct avltree_root {
+
+
+/*!
+ *
+ * \def cdsl_avltreeMaxDepth(root)
+ * \brief Get max depth of the tree
+ * \param[in] root pointer to the root of tree
+ * \return max. depth of the tree
+ *
+ *
+ * \def cdsl_avltreeTraverse(root, cb, order, arg)
+ * \brief Traverse all the items within the tree, cb is callback function
+ * \param[in] root pointer to the root of tree
+ * \param[in] cb callback function called for each item of the tree
+ * \param[in] order incremental / decremental traversal order
+ * \param[in] arg argument which passed as argument when callback is invoked
+ *
+ *
+ * \def cdsl_avltreeTraverseTarget(root,cb,key, arg)
+ * \brief Traverse all the parent item to the target node with given key value
+ * \param[in] root pointer to the root of tree
+ * \param[in] cb callback function called for each item of the tree
+ * \param[in] key key value for the target
+ * \param[in] arg argument which passed as argument when callback is invoked
+ *
+ * \def cdsl_avltreeSize(root)
+ * \brief Get total item count of the tree
+ * \param[in] root pointer to the root of tree
+ * \return total item count of the tree
+ *
+ * \def cdsl_avltreePrint(root, print)
+ * \brief Print root structure of tree
+ * \param[in] root pointer to the root of tree
+ * \param[in] print printer callback which called with arg
+ *
+ * \def  cdsl_avltreeIsEmpty(root)
+ * \brief Check whether the tree is empty or not
+ * \param[in] root pointer to the root of tree
+ * \return return true, if there is no item on the tree, otherwise return false
+ *
+ * \def cdsl_avltreeTop(root)
+ * \brief Get root item of the tree (not very meaningful except for heap)
+ * \param[in] root pointer to the root of tree
+ * \return top (entry) item of the tree
+ *
+ * \def cdsl_avltreeGoLeft(cur)
+ * \brief Peek left child of current item
+ * \param[in] cur pointer to tree node
+ * \return left child of the given tree node
+ *
+ * \def cdsl_avltreeGoRight(cur)
+ * \brief Peek right child of current item
+ * \param[in] cur pointer to tree node
+ * \return right child of the given tree node
+ *
+ * \def cdsl_avltreeMin(root)
+ * \brief Peek item with minimum key value among the items in the tree
+ * \param[in] root pointer to tree root
+ * \return item with minimum key value
+ *
+ * \def cdsl_avltreeMax(root)
+ * \brief Peek item with maximum key value among the items in the tree
+ * \param[in] root pointer to tree root
+ * \return item with maximum key value
+ *
+ * \def cdsl_avltreeUpdate(root,nitem)
+ * \brief Replace item with new one with same key value
+ * \param[in] root pointer to tree root
+ * \param[in] nitem new item
+ * \return old item replaced by nitem
+ *
+ * \def cdsl_avltreeDelete(root, key)
+ * \brief Delete item with the key
+ * \param[in] root pointer to tree root
+ * \param[in] key value for item to be deleted
+ * \return deleted item
+ *
+ * \def cdsl_avltreeDeleteMin(root)
+ * \brief Delete item with minimum key value among the items in the tree
+ * \param[in] root pointer to tree root
+ * \return item with minimum key value
+ *
+ * \def cdsl_avltreeDeleteMax(root)
+ * \brief Delete item with maximum key value among the items in the tree
+ * \param[in] root pointer to tree root
+ * \return item with maximum key value
+ */
+
+/*!
+ * \brief tree node type definition
+ */
+typedef struct cdsl_avlnode avltreeNode_t;
+
+/*!
+ * \brief tree root for AVL tree
+ */
+typedef struct cdsl_avlroot {
 	union {
 		base_treeRoot_t  base_root;
 		avltreeNode_t *entry;
 	};
-	int balance_factor;
+	int balance_factor;   ///< balance factor for AVL tree
 } avltreeRoot_t;
 
-struct avltree_node {
+/*!
+ * \brief tree node for AVL tree
+ */
+struct cdsl_avlnode {
 	union {
-		base_treeNode_t base_node;
+		struct base_tree_node base_node;
 		struct {
-			avltreeNode_t* left, right;
+			avltreeNode_t *left;
+			avltreeNode_t *right;
 			trkey_t key;
 		};
 	};
-	int depth;
+	int depth;  ///< node depth within the tree
 };
 
+/*!
+ * \brief Initialize tree root with balance factor
+ * \param[in] rootp pointer to root of the tree
+ * \param[in] bal balance factor for the AVL tree management
+ */
 extern void cdsl_avltreeRootInit(avltreeRoot_t* rootp, int bal);
+
+/*!
+ * \brief Initialize tree node with key value
+ * \param[in] node uninitialized tree node
+ * \param[in] key key value
+ */
 extern void cdsl_avltreeNodeInit(avltreeNode_t* node, trkey_t key);
 
 /*!
- * insert element to tree
- *
+ * \brief Insert element to tree
+ * \param[in] rootp pointer of tree root
+ * \param[in] item new tree node item to be inserted
+ * \param[in] is_set define conflict handling mechanism, if true,
+ *  	replacement performed if the element with same key value exists
+ *  	within the tree, otherwise, added without replacement
+*  \return if is_set is true, return replaced node otherwise, return null
  */
-extern avltreeNode_t* cdsl_avltreeInsert(avltreeRoot_t* rootp,avltreeNode_t* item, BOOL is_set);
+extern avltreeNode_t* cdsl_avltreeInsert(avltreeRoot_t* rootp, avltreeNode_t* item, BOOL is_set);
+
+/*!
+ * \brief Perform lookup operation and return node with given key, if there are multiple items with same key, the first encountered item during lookup operation will be returned
+ * \param[in] rootp pointer of tree root
+ * \param[in] key key value for the target node
+ * \return found node (\ref avltreeNode_t) with given key
+ */
 extern avltreeNode_t* cdsl_avltreeLookup(avltreeRoot_t* rootp,trkey_t key);
+
+/*!
+ * \brief Delete item with or without replacement
+ * \param[in] rootp pointer of tree root
+ * \param[in] key key value for the target node
+ * \param[in] replacer callback function called when the item to be deleted is found
+ * \param[in] cb_arg callback argument for replacer
+ * \return deleted item
+ * \sa base_tree_replacer_t
+ */
 extern avltreeNode_t* cdsl_avltreeDeleteReplace(avltreeRoot_t* rootp,trkey_t key, base_tree_replacer_t replacer, void* cb_arg);
+
+/*!
+ * \brief Delete item with minimum key value within the tree
+ * \param[in] rootp pointer of tree root
+ * \param[in] replacer callback function called when the item to be deleted is found
+ * \param[in] cb_arg callback argument for replacer
+ * \return deleted item
+ * \sa base_tree_replacer_t
+ */
 extern avltreeNode_t* cdsl_avltreeDeleteMinReplace(avltreeRoot_t* rootp, base_tree_replacer_t replacer, void* cb_arg);
+
+/*!
+ * \brief Delete item with maximum key value within the tree
+ * \param[in] rootp pointer of tree root
+ * \param[in] replacer callback function called when the item to be deleted is found
+ * \param[in] cb_arg callback argument for replacer
+ * \return deleted item
+ * \sa base_tree_replacer_t
+ */
 extern avltreeNode_t* cdsl_avltreeDeleteMaxReplace(avltreeRoot_t* rootp, base_tree_replacer_t replacer, void* cb_arg);
+
+/*!
+ * @}
+ */
+
 
 #if defined(__cplusplus)
 }
--- source/cdsl_avltree.c
+++ source/cdsl_avltree.c
@@ -7,6 +7,142 @@
 
 #include "cdsl_avltree.h"
 
+
+#define DIR_LEFT          ((uint8_t) 1)
+#define DIR_RIGHT         ((uint8_t) 2)
+
+#define PATTERN_MASK      ((uint8_t) 0x3F)
+#define LEFTRIGTH_PATTERN ((uint8_t) (DIR_RIGHT << 2) | DIR_LEFT)
+#define LEFTLEFT_PATTERN  ((uint8_t) (DIR_LEFT << 2) | DIR_LEFT)
+#define RIGHTLEFT_PATTERN ((uint8_t) (DIR_LEFT << 2) | DIR_RIGHT)
+#define RIGHTRIGHT_PATTER ((uint8_t) (DIR_RIGHT << 2) | DIR_RIGHT)
+
+static avltreeNode_t* insert_rc(int bal, avltreeNode_t* sub_root_c, avltreeNode_t* item, uint8_t *rc_dir, avltreeNode_t** replaced);
+static avltreeNode_t* rotate_right(avltreeNode_t* sub_root);
+static avltreeNode_t* rotate_left(avltreeNode_t* sub_root);
+
+
+void cdsl_avltreeRootInit(avltreeRoot_t* rootp, int bal) {
+	if(!rootp) {
+		return;
+	}
+	rootp->balance_factor = bal;
+	rootp->entry = NULL;
+}
+
+void cdsl_avltreeNodeInit(avltreeNode_t* node, trkey_t key) {
+	if(!node) {
+		return;
+	}
+	node->depth = 0;
+	node->key = key;
+	node->left = node->right = NULL;
+}
+
+/*!
+ * insert element to tree
+ *
+ */
+avltreeNode_t* cdsl_avltreeInsert(avltreeRoot_t* rootp,avltreeNode_t* item, BOOL is_set) {
+	if(!rootp || !item) {
+		return NULL;
+	}
+	uint8_t dir = 0;
+	avltreeNode_t* replaced = NULL;
+	rootp->entry = insert_rc(rootp->balance_factor, rootp->entry, item, &dir, is_set? &replaced : NULL);
+	return replaced;
+}
+
+avltreeNode_t* cdsl_avltreeLookup(avltreeRoot_t* rootp,trkey_t key) {
+
+}
+
+avltreeNode_t* cdsl_avltreeDeleteReplace(avltreeRoot_t* rootp,trkey_t key, base_tree_replacer_t replacer, void* cb_arg) {
+
+}
+
+avltreeNode_t* cdsl_avltreeDeleteMinReplace(avltreeRoot_t* rootp, base_tree_replacer_t replacer, void* cb_arg) {
+
+}
+
+avltreeNode_t* cdsl_avltreeDeleteMaxReplace(avltreeRoot_t* rootp, base_tree_replacer_t replacer, void* cb_arg) {
+
+}
+
+
+static avltreeNode_t* insert_rc(int bal, avltreeNode_t* sub_root_c, avltreeNode_t* item, uint8_t *rc_dir,  avltreeNode_t** replaced) {
+	if(!sub_root_c) {
+		item->depth = 1;
+		return item;
+	}
+	if(sub_root_c->key > item->key) {
+		sub_root_c->left = insert_rc(bal, sub_root_c->left, item, rc_dir, replaced);
+		sub_root_c->depth = sub_root_c->left->depth + 1;
+		*rc_dir |= DIR_LEFT;
+	} else if(sub_root_c->key < item->key) {
+		sub_root_c->right = insert_rc(bal, sub_root_c->right, item, rc_dir, replaced);
+		sub_root_c->depth = sub_root_c->right->depth + 1;
+		*rc_dir |= DIR_RIGHT;
+	} else {
+		if(*replaced) {
+			*replaced = sub_root_c;
+			item->left = sub_root_c->left;
+			item->right = sub_root_c->right;
+			item->depth = sub_root_c->depth;
+			return item;
+		} else {
+			sub_root_c->left = insert_rc(bal, sub_root_c->left, item, rc_dir, replaced);
+			sub_root_c->depth = sub_root_c->left->depth + 1;
+			*rc_dir |= DIR_LEFT;
+		}
+	}
+	switch((*rc_dir) & PATTERN_MASK) {
+	case RIGHTLEFT_PATTERN:
+		/*
+		 *
+		 */
+		if(!sub_root_c->left) {
+			if(sub_root_c->right->depth > bal) {
+				sub_root_c->right->left = rotate_right(sub_root_c->right->left);
+				sub_root_c->right = rotate_left(sub_root_c->right);
+			}
+		} else {
+			if(sub_root_c->right->depth - sub_root_c->left->depth > bal) {
+			}
+		}
+		break;
+	case RIGHTRIGHT_PATTER:
+		break;
+	case LEFTLEFT_PATTERN:
+		break;
+	case LEFTRIGTH_PATTERN:
+		break;
+	}
+	*rc_dir <<= 2;
+	return sub_root_c;
+}
+
+static avltreeNode_t* rotate_right(avltreeNode_t* sub_root) {
+	if(!sub_root) {
+		return sub_root;
+	}
+	avltreeNode_t* nroot = sub_root->left;
+	sub_root->left = nroot->right;
+	nroot->right = sub_root;
+	return nroot;
+}
+
+static avltreeNode_t* rotate_left(avltreeNode_t* sub_root) {
+	if(!sub_root) {
+		return sub_root;
+	}
+	avltreeNode_t* nroot = sub_root->left;
+	sub_root->right = nroot->left;
+	nroot->left = sub_root;
+	return nroot;
+}
+
+
 void cdsl_avltreeRootInit(avltreeRoot_t* rootp, int bal) {
 	if(!rootp) {
 		return;
