f6b60d7535bc62b7a961c0b1358275eb2973e483
fritzprix <innocentevil0914@gmail.com>
2018-02-18 21:35:27 +0900

    - clean up legacy code

--- source/cdsl_heap.c
+++ source/cdsl_heap.c
@@ -39,14 +39,14 @@ void cdsl_heapNodeInit(heapNode_t* node, trkey_t key) {
 	node->key = key;
 }
 
-int cdsl_heapEnqueue(heapRoot_t* rootp,heapNode_t* item){
+BOOL cdsl_heapEnqueue(heapRoot_t* rootp,heapNode_t* item){
 	item->left = item->right = NULL;
 	if(!rootp) {
-		return (1 < 0);
+		return FALSE;
 	}
 	rootp->entry = insert_from_bottom_rc(rootp,rootp->entry,item);
 	rootp->dir = !rootp->dir;
-	return (1 > 0);
+	return TRUE;
 }
 
 heapNode_t* cdsl_heapDeqeue(heapRoot_t* rootp){
@@ -119,7 +119,6 @@ static heapNode_t* heapify(heapNode_t* current,heapNode_t* child) {
 		child->right = current;
 		current->left = left;
 		current->right = right;
-		child->right = current;
 		return child;
 	} else {
 		left = child->left;
@@ -128,7 +127,6 @@ static heapNode_t* heapify(heapNode_t* current,heapNode_t* child) {
 		child->right = current->right;
 		current->left = left;
 		current->right = right;
-		child->left = current;
 		return child;
 	}
 }
