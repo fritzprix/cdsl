e12c89f166c8a17109f34255fe2b122a2e0ed7f6
AI제품기획팀 Lee Doowoong <dw.lee@sk.com>
2018-03-06 10:29:10 +0900

    adding serialization support for tree series -add serialization support for avltree

--- source/test/cdsl_avltree_test.c
+++ source/test/cdsl_avltree_test.c
@@ -7,7 +7,10 @@
 
 #include "cdsl_avltree_test.h"
 #include "cdsl_avltree.h"
+#include "cdsl_hash.h"
 #include "base_tree.h"
+#include "file_serializer.h"
+#include "serializer_test.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,6 +19,27 @@ static avltreeNode_t replace;
 static trkey_t keys[TEST_SIZE];
 
 static DECLARE_PRINTER(avlnode_printer);
+
+typedef struct person {
+	avltreeNode_t node;
+	char          name[20];
+}person_t;
+
+static person_t people[SER_SIZE];
+
+
+
+static DECLARE_SERIALIZER_ON_ERROR(onSerError);
+static DECLARE_SERIALIZER_ON_NODE(onSerNode);
+static DECLARE_SERIALIZER_ON_COMPLETE(onSerDone);
+
+
+static cdsl_serializerUsrCallback_t callback = {
+		.on_complete = onSerDone,
+		.on_node_to_data = onSerNode,
+		.on_error = onSerError
+};
+
 
 BOOL cdsl_avltreeDoTest(void) {
 	avltreeRoot_t root;
@@ -200,7 +224,91 @@ BOOL cdsl_avltreeDoTest(void) {
 		PRINT_ERR("wrong replacement\n");
 		return FALSE;
 	}
+
+
+
+
+	for(i = 0;i < SER_SIZE;i++) {
+		cdsl_avltreeNodeInit(&people[i].node, sbdm_hash((unsigned char*) names[i]));
+		strcpy(people[i].name, names[i]);
+		cdsl_avltreeInsert(&aroot, &people[i].node, FALSE);
+	}
+
+
+	if(cdsl_avltreeSize(&aroot) != SER_SIZE) {
+		return FALSE;
+	}
+	file_serializer_t fser;
+	file_serializerOpen(&fser, SERIALIZE_FILE_NAME);
+	cdsl_avltreeSerialize(&aroot, &fser, &callback);
+	file_serializerClose(&fser);
+
+	avltreeRoot_t nroot;
+	cdsl_avltreeRootInit(&nroot,2);
+
+
+	file_deserializer_t desr;
+	if(file_deserializerOpen(&desr, SERIALIZE_FILE_NAME) != OK) {
+		PRINT("OPEN Failed \n");
+	}
+	const cdsl_memoryMngt_t mmngt = GET_DEFAULT_MMNGT();
+	cdsl_avltreeDeserialize(&nroot, &desr, &mmngt);
+	file_deserializerClose(&desr);
+
+	if(cdsl_avltreeCompare(&nroot, &aroot) != 0) {
+		return FALSE;
+	}
+
+	person_t apeople[5];
+	person_t npeople[5];
+	const char* another_names[5] = {
+			"Alice",
+			"Bob",
+			"Smith",
+			"Sam",
+			"Jack"
+	};
+
+	for(i = 0;i < 5;i++) {
+		cdsl_avltreeNodeInit(&apeople[i].node, sbdm_hash((unsigned char*) another_names[i]));
+		cdsl_avltreeNodeInit(&npeople[i].node, sbdm_hash((unsigned char*) another_names[i]));
+
+		strcpy(apeople[i].name, another_names[i]);
+		strcpy(npeople[i].name, another_names[i]);
+
+		cdsl_avltreeInsert(&aroot, &apeople[i].node, FALSE);
+		cdsl_avltreeInsert(&nroot, &npeople[i].node, FALSE);
+	}
+
+	if(cdsl_avltreeCompare(&nroot, &aroot) != 0) {
+		return FALSE;
+	}
+
+	cdsl_avltreeDeleteMin(&aroot);
+	if(cdsl_avltreeCompare(&nroot, &aroot) == 0) {
+		return FALSE;
+	}
+
 	return TRUE;
+}
+
+
+
+static DECLARE_SERIALIZER_ON_ERROR(onSerError) {
+
+}
+
+static DECLARE_SERIALIZER_ON_NODE(onSerNode) {
+	person_t* person = container_of(node, person_t, node);
+	*size = offsetof(person_t, name) + (strlen(person->name) * sizeof(char));
+	if((*size % 2) != 0) {
+		*size += 1;
+	}
+	return (void*) node;
+}
+
+static DECLARE_SERIALIZER_ON_COMPLETE(onSerDone) {
+	// NOP
 }
 
 static DECLARE_PRINTER(avlnode_printer) {
